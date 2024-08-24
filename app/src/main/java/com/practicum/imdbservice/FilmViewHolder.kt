ce2fbb024356be0735f671968ab43a041463ca5a
Michel-mihim <ya-mihim@yandex.ru>
2024-08-24 17:45:04 +0300

    4_viewholder_added

---
+++ app/src/main/java/com/practicum/imdbservice/FilmViewHolder.kt
@@ -0,0 +1,19 @@
+package com.practicum.imdbservice
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FilmViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(LayoutInflater.from(parent.context)
+    .inflate(R.layout.film_list_item, parent,false)) {
+
+    var film_name: TextView = itemView.findViewById(R.id.film_name)
+    var film_information: TextView = itemView.findViewById(R.id.film_information)
+
+    fun bind(film: Film) {
+        film_name.text = film.name
+        film_information.text = film.information
+    }
+
+}
