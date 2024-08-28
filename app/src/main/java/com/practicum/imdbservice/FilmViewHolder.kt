ef468af34d7d1a03b9241a501dc88d35ecc3f719
AndroidMaster <ya-mihim@yandex.ru>
2024-08-28 21:02:22 +0300

    10_glide_works

--- app/src/main/java/com/practicum/imdbservice/FilmViewHolder.kt
+++ app/src/main/java/com/practicum/imdbservice/FilmViewHolder.kt
@@ -1,25 +1,33 @@
 package com.practicum.imdbservice
 
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class FilmViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val film_name: TextView
     private val film_information: TextView
+    private val film_banner: ImageView
 
     init {
         film_name = itemView.findViewById(R.id.film_name)
         film_information = itemView.findViewById(R.id.film_information)
+        film_banner = itemView.findViewById(R.id.film_banner)
     }
 
     fun bind(film: Film) {
         film_name.text = film.title
         film_information.text = film.description
-        Glide.with(itemView.context).load(film.image).placeholder()
+        Glide.with(itemView.context)
+            .load(film.image)
+            .placeholder(R.drawable.placeholder)
+            .centerInside().into(film_banner)
     }
 
 }
