4ebe1bcfd358942fb655ef35cf91c20a0d7fddaf
AndroidMaster <ya-mihim@yandex.ru>
2025-01-09 00:54:01 +0300

    moxy_ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    kotlin("kotlin-kapt")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
